d028c1f827c9fbb2a95cf9ccc7b621c0a11e0581
Quinn Royston <quinn.royston@gmail.com>
2020-06-13 10:36:46 -0700

    Updated to accept post requests from the form

--- server/server.js
+++ server/server.js
@@ -6,11 +6,24 @@ const app = express();
 //MIDDLEWARE
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '../client')));
-
+app.use(express.json())
+app.use(express.urlencoded())
 //ROUTES
 app.get('/', (req, res) => {
   res.sendFile('index')
 });
+
+app.post('/', (req, res) => {
+  console.log('post request submitted')
+  jsonToCSV(req.body["json-form"])
+  //
+  res.redirect('/')
+});
+//JSON to CSV function
+var jsonToCSV = (jsonData) => {
+  console.log('jsonData: ', jsonData)
+
+}
 
 //SERVER PORT
 const port = process.env.PORT || 3000;
